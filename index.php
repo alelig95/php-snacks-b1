dc7d4ea881501acc02ad068caec58076e4da739e
Alessandro Liggieri <smmb@live.it>
2021-02-26 16:29:48 +0100

    Primo push

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+$matches = [
+    "match1" => [
+        "squadraDiCasa" => "Olimpia Milano",
+        "SquadraOspite" => "Cantu",
+        "puntiSquadraDiCasa" => "60",
+        "puntiSquadraOspite" => "55"
+    ],
+    "match2" => [
+        "squadraDiCasa" => "Virtus Bologna",
+        "SquadraOspite" => "Varese",
+        "puntiSquadraDiCasa" => "75",
+        "puntiSquadraOspite" => "72"
+    ],
+    "match3" => [
+        "squadraDiCasa" => "Cremona",
+        "SquadraOspite" => "Trento",
+        "puntiSquadraDiCasa" => "58",
+        "puntiSquadraOspite" => "64"
+    ],
+    "match4" => [
+        "squadraDiCasa" => "Dinamo Sassari",
+        "SquadraOspite" => "Venezia",
+        "puntiSquadraDiCasa" => "76",
+        "puntiSquadraOspite" => "78"
+    ]
+  ];
+
+  for ($i=0; $i < 1; $i++) { 
+    echo '<pre>'; print_r($matches);
+  }
+
+  ?>
+
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+</body>
+</html> */
